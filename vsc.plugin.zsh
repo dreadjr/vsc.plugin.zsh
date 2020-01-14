37bbdf32ad3bb9ae3c8554bb48f2dbb26aad9670
me <dreadjr@users.noreply.github.com>
2020-01-14 15:58:35 -0800

    Update vsc.plugin.zsh

--- vsc.plugin.zsh
+++ vsc.plugin.zsh
@@ -1,24 +1,32 @@
+# if [[ "$OSTYPE" == darwin* ]]; then
+#     local _vsc_darwin_paths > /dev/null 2>&1
+#     _vsc_darwin_paths=(
+#         "/Applications/Visual Studio Code.app/Contents/MacOS/Electron"
+#         "$HOME/Applications/Visual Studio Code.app/Contents/MacOS/Electron"
+#         "/Applications/Visual Studio Code - Insiders.app/Contents/MacOS/Electron"
+#         "$HOME/Applications/Visual Studio Code - Insiders.app/Contents/MacOS/Electron"
+#     )
+#     for _vsc_path in $_vsc_darwin_paths; do
+#         if [[ -a $_vsc_path ]]; then
+#             vscode_open () { "$_vsc_path" $* }
+#             alias vsc=vscode_open
+#             break
+#         fi
+#     done
+# fi
+
+# alias vscc='vsc .'
+
+
+alias vscode='_open_with_vscode'
 if [[ "$OSTYPE" == darwin* ]]; then
-    local _vsc_darwin_paths > /dev/null 2>&1
-    _vsc_darwin_paths=(
-        "/Applications/Visual Studio Code.app/Contents/MacOS/Electron"
-        "$HOME/Applications/Visual Studio Code.app/Contents/MacOS/Electron"
-        "/Applications/Visual Studio Code - Insiders.app/Contents/MacOS/Electron"
-        "$HOME/Applications/Visual Studio Code - Insiders.app/Contents/MacOS/Electron"
-    )
-    for _vsc_path in $_vsc_darwin_paths; do
-        if [[ -a $_vsc_path ]]; then
-            vscode_open () { "$_vsc_path" $* }
-            alias vsc=vscode_open
-            break
-        fi
-    done
+function _open_with_vscode {
+    open -a "Visual Studio Code" $* &> /dev/null
+    if [ $? == 1 ]; then
+      open -a "Visual Studio Code - Insiders" $* &> /dev/null
+    fi
+}
 fi
-
-alias vscc='vsc .'
-
-
-
 
 
 # open -a "Visual Studio Code" &> /dev/null
